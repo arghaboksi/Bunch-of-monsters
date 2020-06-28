2b2d6b0a1e5c5d1a4f6e13387c335646ce95a5fd
unknown <argha.boksi@gmail.com>
2020-06-28 18:36:22 +0530

    Updates

---
+++ precache-manifest.b97e667ecc67e4f2bc941921ca14d910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd46db976992d835c9963796fcdc08d6",
+    "url": "/Bunch-of-monsters/index.html"
+  },
+  {
+    "revision": "11bd785f52bf8c2d7185",
+    "url": "/Bunch-of-monsters/static/css/main.9594edee.chunk.css"
+  },
+  {
+    "revision": "652c3969307ff59b9d1c",
+    "url": "/Bunch-of-monsters/static/js/2.be4c3002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bunch-of-monsters/static/js/2.be4c3002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bd785f52bf8c2d7185",
+    "url": "/Bunch-of-monsters/static/js/main.ac85521b.chunk.js"
+  },
+  {
+    "revision": "1571d9f20168bc1fc482",
+    "url": "/Bunch-of-monsters/static/js/runtime-main.97ed0257.js"
+  }
+]);
